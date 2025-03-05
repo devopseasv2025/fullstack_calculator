dde88d8c5c8c3780890e95ea589f68ae4fabfd0e
Elias Therkildsen <73726902+eliastherkildsen@users.noreply.github.com>
2025-03-05 10:40:38 +0100

    j

--- Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
+++ Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24aae21e0869573e918d0f8ae13d08a513feaf90")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfa2cc54e87edbfafc92e0f106d52955456999b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
