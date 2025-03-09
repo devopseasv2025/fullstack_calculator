a6b3c88d240aedc44fa75f3ea2ccc7df3e11222a
Alexander Gerckens <alexander.gerckens@gmail.com>
2025-03-09 11:59:33 +0100

    fix(MINOR) Testing stuff for the database

---
+++ db/migrations/V1__Test.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE car (
+                     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                     license_plate VARCHAR(20) NOT NULL,
+                     color VARCHAR(20) NOT NULL
+);
