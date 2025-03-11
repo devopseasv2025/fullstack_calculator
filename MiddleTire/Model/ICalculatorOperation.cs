be3135340d08a543de2a2ff8641cb41aaffec9ad
Elias Therkildsen <73726902+eliastherkildsen@users.noreply.github.com>
2025-03-11 09:38:27 +0100

    feat(MiddleTire): CalculatorREPO
    Added ability for calculatorOperations.number2 to be nullable.

--- MiddleTire/Model/ICalculatorOperation.cs
+++ MiddleTire/Model/ICalculatorOperation.cs
@@ -5,7 +5,7 @@ namespace MiddleTire.Model;
 public interface ICalculatorOperation
 {
     int Number1 { get; set; }
-    int Number2 { get; set; }
+    int? Number2 { get; set; }
     ECalculatorOperations Operation { get; set; }
     double? Result { get; set; }
     ECalculators Calculator { get; set; }
@@ -14,7 +14,7 @@ public interface ICalculatorOperation
 public class CalculatorOperation : ICalculatorOperation
 {
     public int Number1 { get; set; }
-    public int Number2 { get; set; }
+    public int? Number2 { get; set; }
     public ECalculatorOperations Operation { get; set; }
     public double? Result { get; set; }
     public ECalculators Calculator { get; set; }
