efeafd7c13190df588cbf844f91f5c9a2c22e8fb
Elias Therkildsen <73726902+eliastherkildsen@users.noreply.github.com>
2025-03-11 09:37:25 +0100

    test: MiddleTire
    Created test sln for middletire.

---
+++ Calculator.Tests/Repository/CalculatorRepoTest.cs
@@ -0,0 +1,16 @@
+﻿using MiddleTire.Repository;
+using NUnit.Framework;
+
+namespace Calculator.Tests.Repository;
+
+[TestFixture]
+[TestOf(typeof(CalculatorRepo))]
+public class CalculatorRepoTest
+{
+
+    [Test]
+    public void add()
+    {
+        
+    }
+}
