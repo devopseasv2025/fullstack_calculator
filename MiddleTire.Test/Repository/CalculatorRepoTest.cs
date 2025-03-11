5aea83f9292e8a05aab20657fcbef28eff6858b4
Elias Therkildsen <73726902+eliastherkildsen@users.noreply.github.com>
2025-03-11 10:16:57 +0100

    test(MiddleTire): CalculatorREPO
    Added tests for calculate method in calcrepo

---
+++ MiddleTire.Test/Repository/CalculatorRepoTest.cs
@@ -0,0 +1,38 @@
+﻿using System.Runtime.InteropServices.JavaScript;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MiddleTire.Enums;
+using MiddleTire.Model;
+using MiddleTire.Repository;
+using NUnit.Framework;
+
+namespace MiddleTire.Test.Repository;
+// ARRANGE
+// ACT
+// ASSERT
+[TestFixture]
+[TestOf(typeof(CalculatorRepo))]
+public class CalculatorRepoTest
+{
+
+    [Test]
+    public void Operations_returns_obj_IResult_valid()
+    {
+        // ARRANGE
+        ICalculatorRepo calculatorRepo = new CalculatorRepo();
+        ICalculatorOperation operation = new CalculatorOperation
+        {
+            Number1 = 1,
+            Number2 = 2,
+            Operation = ECalculatorOperations.Addition,
+            Calculator = ECalculators.CashedCalculator
+        };
+
+        // ACT
+        var result = calculatorRepo.Calculate(operation);
+
+        // ASSERT
+        Assert.That(result, Is.InstanceOf<IResult>());
+    }
+    
+}
