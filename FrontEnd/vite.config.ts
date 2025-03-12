4c6e438a6b16aa9494325ac183b28aa607719a11
Elias Therkildsen <73726902+eliastherkildsen@users.noreply.github.com>
2025-03-12 15:54:57 +0100

    (feat) Implement frontend into docker-compose.yaml and configured dockerfile for frontend (MINOR)

--- FrontEnd/vite.config.ts
+++ FrontEnd/vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -14,5 +16,28 @@ export default defineConfig({
     strictPort: true,
     host: true,
     origin: "http://0.0.0.0:8080",
+  },
+  resolve: {
+    alias: {
+      crypto: 'crypto-browserify',
+      stream: 'stream-browserify',
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: 'globalThis',
+      },
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          process: true,
+          buffer: true,
+        }),
+        NodeModulesPolyfillPlugin(),
+      ],
+    },
+  },
+  define: {
+    'process.env': {}, // Ensures process.env variables are available
   },
 });
