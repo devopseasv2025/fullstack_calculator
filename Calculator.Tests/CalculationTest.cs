d883d8750b1c4f0522c0510a43d05af13e55bdc6
Elias Therkildsen <73726902+eliastherkildsen@users.noreply.github.com>
2025-03-12 11:20:10 +0100

    test(Calculator): cashedCalculator.cs

---
+++ Calculator.Tests/CalculationTest.cs
@@ -0,0 +1,42 @@
+﻿using Calculator;
+using NUnit.Framework;
+
+namespace Calculator.Tests;
+
+[TestFixture]
+[TestOf(typeof(CachedCalculator.Calculation))]
+public class CalculationTest
+{
+    
+    private class TestableCalculation : CachedCalculator.Calculation
+    {
+        public TestableCalculation(string operation, int a, int? b = null)
+            : base(operation, a, b) { }
+    }
+
+    [Test]
+    public void GetKey_ReturnsCorrectString()
+    {
+        // Arrange
+        var calculation = new TestableCalculation("+", 5, 3);
+
+        // Act
+        string key = calculation.GetKey();
+
+        // Assert
+        Assert.That("5+3", Is.EqualTo(key));
+    }
+
+    [Test]
+    public void GetKey_ReturnsCorrectString_WhenBIsNull()
+    {
+        // Arrange
+        var calculation = new TestableCalculation("-", 10, null);
+
+        // Act
+        string key = calculation.GetKey();
+
+        // Assert
+        Assert.That("10-", Is.EqualTo(key));
+    }
+}
