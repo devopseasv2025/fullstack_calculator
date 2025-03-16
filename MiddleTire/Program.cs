b7da0889827ac062853ba250ae2256c66c61c21a
Elias Therkildsen <73726902+eliastherkildsen@users.noreply.github.com>
2025-03-16 09:43:54 +0100

    (feat)(PATCH) made API handle cors.

--- MiddleTire/Program.cs
+++ MiddleTire/Program.cs
@@ -18,11 +18,24 @@ public static class Program
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
-        builder.Services.AddScoped<ICalculatorRepo, CalculatorRepo>();
+        
+        builder.Services.AddCors(options =>
+        {
+            options.AddPolicy("AllowAll",
+                policy => policy.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+        });
 
+        
+        builder.Services.AddScoped<ICalculatorRepo, CalculatorRepo>();
+        
 
         var app = builder.Build();
 
+        app.UseCors("AllowAll");
+
+        
         // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
