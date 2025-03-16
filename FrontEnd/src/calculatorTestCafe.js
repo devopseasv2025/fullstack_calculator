e909038f464e1bacb98f9ef07a490265715d0134
Alexander Gerckens <112266323+Gercken@users.noreply.github.com>
2025-03-16 14:02:02 +0100

    fix(PATCH): Fixed TestCafe

--- FrontEnd/src/calculatorTestCafe.js
+++ FrontEnd/src/calculatorTestCafe.js
@@ -5,5 +5,5 @@ fixture("Calculator tests").page("http://localhost:8080/");
 test("Select Chaced Calculator", async t => {
     await t
         .click(Selector("#root > div:nth-child(1) > button:nth-child(5)"))
-        .expect(Selector('p').innerText).contains('CashedCalculator');
+        .expect(Selector('p').innerText).contains('1');
 });
