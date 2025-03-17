959f787cca6de8132635b85b5c6ab2125f3518df
Alexander Gerckens <112266323+Gercken@users.noreply.github.com>
2025-03-17 10:29:15 +0100

    fix (PATCH) Reinstated spike test

--- MiddleTire.Test.Loadtesting/scripts/spike_test.js
+++ MiddleTire.Test.Loadtesting/scripts/spike_test.js
@@ -4,16 +4,16 @@ export let options = {
     insecureSkipTLSVerify: true,
     noConnectionReuse: false,
     stages: [
-        { duration: '10s', target: 100 }, 
-
+        { duration: '10s', target: 100 },
+        { duration: '1m', target: 100 },
+        { duration: '10s', target: 1400 },
+        { duration: '3m', target: 1400 },
+        { duration: '10s', target: 100 },
+        { duration: '3m', target: 100 },
+        { duration: '10s', target: 0 },
     ],
     
-    /*        { duration: '1m', target: 100 }, 
-        { duration: '10s', target: 1400 }, 
-        { duration: '3m', target: 1400 }, 
-        { duration: '10s', target: 100 }, 
-        { duration: '3m', target: 100 }, 
-        { duration: '10s', target: 0 }, */
+            
     
     thresholds: {
         http_req_failed: ['rate<0.001'], // http errors should be less than 0.1%
